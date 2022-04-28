a588e995298f04a8ffd522c4e6a7357874196b80
indiraavendano <indiraavendano@koverse.com>
2022-04-28 11:37:17 -0400

    Map with Markers updated every 25 seconds

--- oauth-skeleton2/client/src/components/Homepage/Homepage.js
+++ oauth-skeleton2/client/src/components/Homepage/Homepage.js
@@ -1,10 +1,24 @@
-import React, { useEffect, useState, useContext, useRef} from "react";
+import React, { useEffect, useState, useContext, useRef, useMemo} from "react";
 import axios from 'axios';
 import { Button} from '@material-ui/core';
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../Auth/AuthContext";
 import moment from "moment";
-import ReactMapGL, {Marker} from 'react-map-gl'
+import ReactMapGL, {Marker, NavigationControl, FullscreenControl} from 'react-map-gl'
+
+const fullscreenControlStyle = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  padding: "10px"
+};
+
+const navStyle = {
+  position: "absolute",
+  top: 36,
+  left: 0,
+  padding: "10px"
+};
 
 const Homepage = () => {
 
@@ -15,14 +29,13 @@ const Homepage = () => {
     const navigate = useNavigate();
     const {loggedIn} = useContext(AuthContext);
 
-    const [wolfData, setWolfData] = useState(null);
     const [adsbData, setAdsbData] = useState([]);
     const [dataTimer, setDataTimer] = useState({});
     const [lastFetchTime, setLastFetchTime] = useState(
         moment().subtract("5", "minutes").format("YYYY-MM-DD HH:mm:ss")
       );
-    //const REFRESH_TIME = 25000;
-    const REFRESH_TIME = 10000;
+    const REFRESH_TIME = 25000;
+    //const REFRESH_TIME = 10000;
 
     const MAPBOX_TOKEN = 'pk.eyJ1IjoiaW5kaXJhamhlbm55IiwiYSI6ImNsMmc1NGtlYzAwbDczamx3OXpnZ3NrNzMifQ.ei_QTtV_inPDuU_SKQ1BBA';
 
@@ -31,9 +44,13 @@ const Homepage = () => {
       latitude: 39.742043,
       width: "100%",
       height: "100%",
-      zoom: 10,
+      zoom: 6,
+      bearing: 0,
+      pitch: 0
   })
     const mapRef = useRef();
+
+    const blueMarker = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
 
     const logout = () => {
       console.log("Remove token and log out");
@@ -115,7 +132,42 @@ const Homepage = () => {
           let rand = Math.random() * 15;
           rand = Math.floor(rand);
           setRecordOne(JSON.stringify(res.data.records[rand]))
-          setAdsbData(res.data.records);
+          // instead of setting like this, map the objects within array of objects to new
+          // array with an object you designed on your own, it will help when making layers
+          console.log(res.data.records)
+          let preFlightData = res.data.records.map((record) => {
+
+              return {
+                coordinates: [
+                  Number(record.lon),
+                  Number(record.lat),
+                  Number(record.alt_baro),
+                ],
+                callsign: record.flight,
+                track: record.track,
+                hex: record.hex,
+                type: "Plane",
+                details: record.flight,
+                category: record.category,
+                nav_heading: record.nav_heading,
+                squawk: record.squawk,
+                IngestTime: record.flight_aware_ts,
+              };
+          });
+
+          preFlightData = preFlightData.filter(function (element) {
+            return element !== undefined;
+          });
+
+          const dataAsObj = {};
+            let sortedData = preFlightData;
+            sortedData.forEach((entry) => (dataAsObj[entry["hex"]] = entry));
+            sortedData = preFlightData.map(
+              (entry) => dataAsObj[entry["hex"]] || entry
+            );
+  
+            // console.log(sortedData)
+            setAdsbData(sortedData);
           setLastFetchTime(moment().format("YYYY-MM-DD HH:mm:ss"));
   
         })
@@ -139,13 +191,36 @@ const Homepage = () => {
     };
   }, [dataTimer]);
 
+  // const adsbLayer =
+  // adsbData &&
+  // new ScenegraphLayer({
+  //   loadOptions: GLBLoader,
+  //   id: "plane-layer",
+  //   data: adsbData,
+  //   pickable: true,
+  //   sizeScale: 30,
+  //   scenegraph: MODEL_URL,
+  //   _animations: ANIMATIONS,
+  //   sizeMinPixels: 0.1,
+  //   sizeMaxPixels: 10,
+  //   // need to add these coordinates in
+  //   getPosition: (d) => d.coordinates,
+  //   getOrientation: (d) => [0, -d.track || 0, 90],
+  //   onClick: (info, event) => showPlaneData(info, event),
+  //   onHover: ({ object, x, y }) => {
+  //     setActivePlane({ object, x, y });
+  //   },
 
+  //   getColor: (d) => (d.category == "A1" ? "red" : "white"),
+  //   updateTriggers: {
+  //     getFillColor: ["red", "white"],
+  //   },
+  // });
 
     return (
         <div>
             {loggedIn && 
                 <>
-                    <p>Homepage</p>
                     <b>User Credentials: </b>
                     <p>{userDisplayName}</p>
                     <p>{userEmail}</p>
@@ -155,20 +230,30 @@ const Homepage = () => {
                     <Button style={{color: 'white', background: 'gray'}}
                     onClick={()=> {getData3()}}
                     >Get Data</Button>
-                    <>{recordOne}</>
-
                     <div style={{ height: "100vh" }}>
                     <ReactMapGL
                       {...viewport}
+                      width="100vw"
+                      height="100vh"
                       ref={mapRef}
                       mapStyle="mapbox://styles/mapbox/dark-v9"
                       mapboxAccessToken={MAPBOX_TOKEN}
-                      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+                      onViewportChange={(viewport) => setViewport(viewport)}
                       >
-                    markers here
-                    <Marker longitude={-104.991531} latitude={39.742043} anchor="bottom" >
-                      <img src="http://maps.google.com/mapfiles/ms/icons/blue-dot.png" />
-                    </Marker>
+                    {console.log(adsbData)}
+                    {adsbData != null && adsbData.map((data, i) => (
+                      <Marker  key={`marker-${i}`} longitude={data.coordinates[0]} latitude={data.coordinates[1]} anchor="bottom" >
+                        {console.log("adsbData length: " + adsbData.length)}
+                        <img src="http://maps.google.com/mapfiles/ms/icons/blue-dot.png" />
+                      </Marker>
+                      
+                    ))}
+                    <div className="fullscreen" style={fullscreenControlStyle}>
+                      <FullscreenControl />
+                    </div>
+                    <div className="nav" style={navStyle}>
+                      <NavigationControl />
+                    </div>
                     </ReactMapGL>        
                     </div>
                 </> 

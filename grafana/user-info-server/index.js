e07abde6a034f4cf6f0eedfd3f5091792f5f40bd
Garrett Criss <astrolope@gmail.com>
2022-05-18 12:08:41 -0700

    Grafana user info

--- grafana/user-info-server/index.js
+++ grafana/user-info-server/index.js
@@ -13,7 +13,6 @@ app.get('/userinfo', async (req, res) => {
 
     const response = await fetch('https://api.staging.koverse.com/me', {
         headers: {'Authorization': req.headers.authorization},
-
     });
     
     const user = JSON.parse(response.headers.get('koverse-user'));
@@ -37,26 +36,6 @@ app.get('/userinfo', async (req, res) => {
     })
 })
 
-app.get('/userinfo/emails', (req, res) => {
-    console.log(req.headers)
-    console.log(req.body)
-    res.send({
-        "sub": "00uid4BxXw6I6TV4m0g3",
-        "name" :"John Doe",
-        "nickname":"Jimmy",
-        "given_name":"John",
-        "middle_name":"James",
-        "family_name":"Doe",
-        "profile":"https://example.com/john.doe",
-        "zoneinfo":"America/Los_Angeles",
-        "locale":"en-US",
-        "updated_at":1311280970,
-        "email":"john.doe@example.com",
-        "email_verified":true,
-        "address" : { "street_address":"123 Hollywood Blvd.", "locality":"Los Angeles", "region":"CA", "postal_code":"90210", "country":"US" },
-        "phone_number":"+1 (425) 555-1212"
-      })
-  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
